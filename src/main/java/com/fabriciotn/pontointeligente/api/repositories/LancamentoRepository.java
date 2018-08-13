30bcd35f18c6a041409b495a01c6219954bee8fc
Fabricio Teixeira Nascimento <fabriciotn82@gmail.com>
2018-08-13 00:15:48 -0300

    Ajuste na interface LancamentoRepository e inclusão das classes de teste.

--- src/main/java/com/fabriciotn/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/com/fabriciotn/pontointeligente/api/repositories/LancamentoRepository.java
@@ -1,12 +1,12 @@
 package com.fabriciotn.pontointeligente.api.repositories;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +15,11 @@ import com.fabriciotn.pontointeligente.api.entities.Lancamento;
 
 @Transactional(readOnly = true)
 @NamedQueries({
-		@NamedQuery(name = "LancamentoRepository.findyByFuncionarioId", 
+		@NamedQuery(name = "LancamentoRepository.findByFuncionarioId", 
 				query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
-	List<Lancamento> findyByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+	List<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
 
-	Page<Lancamento> findyByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
+	Page<Lancamento> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
 }
